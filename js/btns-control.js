d9ad7cd3e39f963ef11756e16bdd5d1e173a5755
YasminDalla <yasmindallarti837@gmail.com>
2020-12-11 21:47:47 -0300

    commit layout2

---
+++ js/btns-control.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.btn-new').click(function(e) {
+        e.preventDefault()
+
+        $('#formulario').load('src/contato/visao/form-contato.html')
+
+
+    })
+
+
+
+
+
+})
